2c5df6d060a7f5336a3edd9f5d58c39bfe19e7ac
Jack Hilton <Jack Hilton>
2017-03-23 16:13:04 -0400

    fixed SQL issues

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Philly Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Barbecue Cheeseburger');
